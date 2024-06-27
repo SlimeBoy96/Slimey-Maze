e14e3ed6c08c2b9c4eaed130212ad41de161e540
SlimeBoy96 <dmr851425@gmail.com>
2024-06-27 12:27:34 -0700

    Update replay.js

--- replay.js
+++ replay.js
@@ -1,1 +1,8 @@
+// Function to reload the page
+function reloadPage() {
+    location.reload();
+}
+
+// Event listener for the replay button
+document.getElementById('replayButton').addEventListener('click', reloadPage);
 
